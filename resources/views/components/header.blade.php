41f79e11df3b0d3808b901010fedc3c97f3f1ad7
iamfrego <mate_freg@tiscali.it>
2021-12-17 15:00:28 +0100

    Header done

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,25 @@
 <header id="site_header">
-    <p>HEADER</p>
+    <div class="upper-header py-2">
+        <div class="container d-flex justify-content-end">
+            <span class="text-white">DC POWER &#8482; VISA &#174;</span>
+            <span class="text-white ps-5">ADDITIONAL DC SITES ▼</span>
+
+        </div>
+    </div>
+    <div class="container d-flex justify-content-between align-items-center pt-3">
+        <img src="./img/dc-logo.png" alt="">
+        <nav class="nav justify-content-center">
+            <a class="nav-link" href="#">CHARACTERS</a>
+            <a class="nav-link" href="#">COMICS</a>
+            <a class="nav-link" href="#">MOVIE</a>
+            <a class="nav-link" href="#">TV</a>
+            <a class="nav-link" href="#">GAMES</a>
+            <a class="nav-link" href="#">COLLECTIBLE</a>
+            <a class="nav-link" href="#">VIDEOS</a>
+            <a class="nav-link" href="#">FANS</a>
+            <a class="nav-link" href="#">NEWS</a>
+            <a class="nav-link" href="#">SHOP</a>
+        </nav>
+
+    </div>
 </header>
