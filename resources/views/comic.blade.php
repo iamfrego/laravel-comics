1a24922bf3d43924c1fb1b422d0b78ca5155f738
iamfrego <mate_freg@tiscali.it>
2021-12-20 17:31:10 +0100

    talent & specs done

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -18,10 +18,10 @@
         </div>
 
         <div class="comic-info mt-5 mb-4">
-            <h2> {{ $comic['title'] }}</h2>
+            <h2 class="darker-brand"> {{ $comic['title'] }} </h2>
         </div>
 
-        <div class="row">
+        <div class="row mb-3">
             <div class="col-8">
                 <div class="price_avaiabile justify-content-between d-flex pt-3 px-3">
                     <div class="price-box justify-content-between d-flex">
@@ -52,7 +52,73 @@
     </div>
 
     <div class="bg-brand-secondary">
-
+        <div class="container pb-5">
+            <div class="row pt-3">
+                <div class="col-6  ">
+                    <h3 class="darker-brand">Talent</h3>
+                    <hr>
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="darker-brand">Art by :</p>
+                        </div>
+                        <div class="col-8">
+                            @foreach($comic['artists'] as $artist)
+                            <span class="brand-color fw-bold"> {{$artist}} </span>
+                            @if(!$loop -> last)
+                            <span>,</span>
+                            @endif
+                            @endforeach
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row mt-3">
+                        <div class="col-4">
+                            <p class="darker-brand"> Written by :</p>
+                        </div>
+                        <div class="col-8 ">
+                            @foreach($comic['writers'] as $writer)
+                            <span class="brand-color fw-bold"> {{$writer}} </span>
+                            @if(!$loop -> last)
+                            <span>,</span>
+                            @endif
+                            @endforeach
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+                <div class="col-6">
+                    <h3 class="darker-brand">Specs</h3>
+                    <hr>
+                    <div class="row mt-3">
+                        <div class="col-4">
+                            <p class="darker-brand">Series :</p>
+                        </div>
+                        <div class="col-8 ">
+                            <p class="text-uppercase brand-color fw-bold">{{ $comic['series'] }}</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="darker-brand">U.S. Price:</p>
+                        </div>
+                        <div class="col-8 ">
+                            <p> {{ $comic['price'] }} </p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="darker-brand">On Sale Date:</p>
+                        </div>
+                        <div class="col-8 ">
+                            <p class="text-uppercase">{{ $comic['sale_date'] }}</p>
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+        </div>
     </div>
 
 
