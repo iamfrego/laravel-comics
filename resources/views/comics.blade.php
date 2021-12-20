8386efbd8c3a171ed589e429177f44033eb0285f
iamfrego <mate_freg@tiscali.it>
2021-12-20 17:55:05 +0100

    Fix Comics Route

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,9 +1,60 @@
 @extends('layouts.app')
 
-@section('page-title', 'Comics')
+@section('page-title', 'Home')
 
 @section('content')
-<div>
-    <h1>Comics page</h1>
+<div class="main-bg">
+    <div class="container">
+        <h2>Current Series</h2>
+        <div class="series row">
+            @foreach($comics as $index => $comic)
+            <div class="col-2">
+                <a href="{{ route('comic', ['id' => $index]) }}">
+                    <div class="card_comics">
+                        <img src="{{ $comic['thumb'] }}" alt="Comics">
+                        <h3>{{ $comic['series'] }}</h3>
+                    </div>
+                </a>
+            </div>
+            @endforeach
+        </div>
+        <div class="button">
+            <button>Load More</button>
+        </div>
+    </div>
+    <div class="menu_dc">
+        <div class="container">
+            <div class="icon">
+                <div class="img">
+                    <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt>
+                </div>
+                <h3>Digital comics</h3>
+            </div>
+            <div class="icon">
+                <div class="img">
+                    <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt>
+                </div>
+                <h3>DC merchandise</h3>
+            </div>
+            <div class="icon">
+                <div class="img">
+                    <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt>
+                </div>
+                <h3>Subscription</h3>
+            </div>
+            <div class="icon">
+                <div class="img">
+                    <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt>
+                </div>
+                <h3>Comic shop locator</h3>
+            </div>
+            <div class="icon">
+                <div class="img">
+                    <img style="height: 40px" src="{{ asset('img/buy-dc-power-visa.svg') }}" alt>
+                </div>
+                <h3>DC power visa</h3>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $comics = config('comics');
-    return view('home', compact('comics'));
+    return view('home');
 })->name('home');
 
 Route::get('/characters', function () {
@@ -23,7 +22,8 @@ Route::get('/characters', function () {
 })->name('characters');
 
 Route::get('/comics', function () {
-    return view('comics');
+    $comics = config('comics');
+    return view('comics',compact('comics'));
 })->name('comics');
 
 Route::get('/movies', function () {
